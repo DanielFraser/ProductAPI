53d86d3d1a6043329ab0fd765d7729bedb672475
Daniel R Fraser <dukeydk@gmail.com>
2019-02-18 23:26:47 -0500

    Initial Commit

---
+++ WebApplication1/Model/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirst.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+
+    
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("Program") { }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApplication1/Products.asmx.cs
@@ -0,0 +1,46 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for Products
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class Products : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<string> names(string prefix)
+        {
+            System.Diagnostics.Debug.WriteLine("a");
+            List<string> temp;
+            using (var context = new MyContext())
+            {
+                temp = context.Products.Select(a => a.name).Where(a => a.Contains(prefix)).ToList();
+            }
+            System.Diagnostics.Debug.WriteLine("b");
+            return temp;
+        }
+
+        public void add()
+        {
+
+        }
+    }
+}
